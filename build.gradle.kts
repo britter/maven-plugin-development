5f1e02bd8d2ad2ef3a1c411a06a0b3b3c343d502
Benedikt Ritter <beneritter@gmail.com>
2020-04-06 18:18:20 +0200

    Bump version number to next development version

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,7 @@ plugins {
 
 group = "de.benediktritter"
 description = "Gradle plugin for developing Apache Maven plugins"
-version = "0.1.0"
+version = "0.1.1-SNAPSHOT"
 
 java {
     sourceCompatibility = JavaVersion.VERSION_1_8
