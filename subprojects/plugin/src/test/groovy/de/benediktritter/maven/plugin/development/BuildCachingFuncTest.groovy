318d80a647db54683a821692bf4887579e9623ee
Benedikt Ritter <beneritter@gmail.com>
2020-10-14 15:39:30 +0200

    Revert "Introduce failures to test build scan comment"
    This reverts commit 0a0f0e92c61af632ed7f3bf0ee3de37159a28ba6.

--- subprojects/plugin/src/test/groovy/de/benediktritter/maven/plugin/development/BuildCachingFuncTest.groovy
+++ subprojects/plugin/src/test/groovy/de/benediktritter/maven/plugin/development/BuildCachingFuncTest.groovy
@@ -37,6 +37,5 @@ class BuildCachingFuncTest extends AbstractPluginFuncTest {
         then:
         result.task(":generateMavenPluginHelpMojoSources").outcome == TaskOutcome.FROM_CACHE
         result.task(":generateMavenPluginDescriptor").outcome == TaskOutcome.FROM_CACHE
-        false
     }
 }
