01f2e222547e18471626cc71efd33622ce0a3ff0
Benedikt Ritter <beneritter@gmail.com>
2021-07-26 22:44:20 +0200

    Use string tasks paths instead of task instances

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,9 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-val publishPlugin = tasks.findByPath(":plugin:publishPlugins")
-val publishDocs = tasks.findByPath(":documentation:gitPublishPush")
-
 tasks.register("release") {
-    dependsOn(publishPlugin, publishDocs)
+    dependsOn(":plugin:publishPlugins", ":documentation:gitPublishPush")
 }
